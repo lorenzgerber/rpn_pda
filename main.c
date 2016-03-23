aef634911dda59d558ce3f6c4121453fe174a636
Lorenz Gerber <genfys@gmail.com>
2016-03-23 04:27:25 +0100

    no real added code, just played with defining some transitions and states

--- main.c
+++ main.c
@@ -27,32 +27,33 @@ int main(int argc, char **argv) {
 
     // Test creating states
     //State *first = calloc(1, sizeof(State));
-    State *first = state_create(0, false, "This is the first state");
-    State *second = state_create(1, true, "This is the second state");
+    State *start = state_create(0, false, "Accept number or blank");
+    State *first = state_create(1, true, "Non-terminated number");
+    State *second = state_create(2, false, "top of stack to be discareded");
+    State *third = state_create(3, true, "Accept number or blank");
 
 
     // Test creating transitions
     // Comment, source, dest, read, pop, push
-    Transition *trans1 = transition_create("this is the transition from a to b",0, 1, isdigit, NULL, pushInput);
-    Transition *trans2 = transition_create("this is the transition from b to c",0, 2, isdigit, isdigit, pushInput);
-    Transition *trans3 = transition_create("this is the transition fron c to d",1, 2, isBlank, isdigit, pushInput);
-
+    Transition *trans1 = transition_create("transition when first input is a blank",2, isBlank, NULL, pushInput);
+    Transition *trans2 = transition_create("this is the transition from b to c",2, isdigit, isBlank, NULL);
+    Transition *trans3 = transition_create("pop the top of the stack, then back to 0",0, NULL, isBlank, NULL);
+    Transition *trans4 = transition_create("first input is a number", 1, isdigit, NULL, pushInput);
+    Transition *trans5 = transition_create("terminating first number", 3, isBlank, NULL, pushInput);
 
 
     // Test adding transitions to state
-    state_addTransition(first, trans1);
-    state_addTransition(first, trans2);
-    printf("state id %d\n", state_getId(first));
-
+    state_addTransition(start, trans1);
+    state_addTransition(start, trans4);
+    state_addTransition(first, trans4);
+    state_addTransition(first, trans5);
     state_addTransition(second, trans3);
 
-    if(!state_isEmpty(first))
-        printf("no longer empty\n");
-
     // Test adding state to pda
+    pda_addState(rpn_pda, start);
     pda_addState(rpn_pda, first);
     pda_addState(rpn_pda, second);
-
+    pda_addState(rpn_pda, third);
 
 
     /*
